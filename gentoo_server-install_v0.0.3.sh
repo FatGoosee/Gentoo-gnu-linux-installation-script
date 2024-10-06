2ddc6b0e48326fe1419741eec90481a2a71555fa
FatGoose <98902645+FatGoosee@users.noreply.github.com>
2024-10-06 10:57:25 +0000

    Switched supported bootmethods to uEFI only
    Added support for unlocking encrypted root with gpg-encrypted keyfile

---
+++ gentoo_server-install_v0.0.3.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+sSCRIPT_VERSION="0.0.3"
+echo "" > serverInstall.log
+
+die ()
+	{
+    	exit 1
+  	}
+
+command ()
+  	{
+		sUNMASK="--autounmask-write --autounmask"
+		"$@"
+		sUNMASK="--verbose=n"
+		if [[ $(etc-update --automode -5) == *"Replacing"* ]]
+			then
+				"$@"		
+			fi
+		
+		printf "\n\n$?:\t$@\n" >> serverInstall.log
+		"$@" &>> serverInstall.log
+  	}
+
+sUNMASK="--autounmask-write --autounmask"
+sEMERGE="emerge ${sUNMASK}"
+serverPkgUse ()
+  	{
+    	echo "Setting package useflags..."
+      		command echo "media-video/pipewire jack-sdk pipewire-alsa gst-plugin-pipewire echo-cancel lv2 modemmanager roc sound-server" > /etc/portage/package.use/pipewire
+      		command echo "app-containers/docker btrfs" > /etc/portage/package.use/docker
+  	}
+ 
+installServerpkgs ()
+  	{
+    	echo "Installing server profile packages..."
+      		if [ ${iPROFILE[1]} == true ]
+      			then
+      				command ${sEMERGE} app-containers/docker app-containers/docker-cli
+      				command libtool --finish /usr/lib64
+      				command systemctl enable docker
+      				command systemctl start docker
+      				command printf "net.ipv4.ip_forward=1\net.ipv6.ip_forward=1" >> /etc/sysctl.d/local.conf
+      				command docker create -t nginx-01 nginx
+      		else
+      				command ${sEMERGE} app-containers/docker app-containers/docker-cli www-server/nginx media-video/wireplumber media-video/pipewire media-libs/libpulse dev-db/postresql net-dns/avahi sys-auth/rtkit
+      				command libtool --finish /usr/lib64
+      			fi
+      		if [ ${iMAC} == "selinux" ]
+      			then
+      				rlpkg -a -r
+      			fi
+  	}
+
+serverPkgUse
+updateWorld
+installServerpkgs
+aliasLine
+writeBashrc
+enableServices
+cleanUP
---
+++ gentoo_setup-install_v0.0.3.sh
@@ -0,0 +1,107 @@
+#! /bin/bash
+sSCRIPT_VERSION="0.0.3"
+echo "" > setupExit1.log
+
+die () 
+	{
+		umount /mnt/gentoo/boot
+		umount /mnt/gentoo/home
+		umount /mnt/gentoo
+		exit 1
+	}
+
+command ()
+	{
+        "$@"
+  		
+		printf "\n\n$?:\t$@\n\t\t" >> setupInstall.log
+		"$@" &>> setupInstall.log
+	}
+
+
+formatDisk ()
+	{
+    	echo "Formating disk..."
+			command parted -s /dev/${iDEVICE} mklabel ${iPARTTABLE}
+			command parted -s /dev/${iDEVICE} mkpart primary ${iBOOT[0]} ${iBOOT[1]} ${iBOOT[2]}
+			command parted -s /dev/${iDEVICE} mkpart primary ${iGENTOO[0]} ${iGENTOO[1]} ${iGENTOO[2]}
+			command parted -s /dev/${iDEVICE} name 1 boot
+			command parted -s /dev/${iDEVICE} name 2 gentoo
+			command parted -s /dev/${iDEVICE} set 1 boot on
+	}
+
+stage3 ()
+	{
+    	command tar xpvf ${sMOUNTPOINT}/stage3-*.tar.xz -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner
+	}
+createFilesystem ()
+	{
+	    echo "Creating filesystem..."
+			command mkfs.vfat -F32 /dev/${iDEVICE}1
+			if [ ${sLUKS[0]} == true ]	#checking if encryption is wanted
+				then
+					iLUKS="mapper/luksdev"
+					if [ ${sLUKS[2]} == true ]	#checking if gpg-encrypted-keyfile is wanted
+						then
+							command printf "\n\n\ny\ncrypt_key\ncrypt_key@encrypt.system\n\nO\n${sLUKS[1]}\n${sLUKS[1]}\n" | gpg --full-generate-key
+							command printf "${sLUKS[1]}" | gpg --export-secret-keys crypt_key --output crypt_key.luks.gpg --encrypt
+							sGPG="$(printf \"${sLUKS[1]}\" | gpg --decrypt crypt_key.luks.gpg)" 
+							command printf "${sGPG}" | cryptsetup luksFormat -q -c aes-xts-plain64 -s 512 /dev/${iDEVICE}2 -
+							command printf "${sGPG}\n${sLUKS[1]}\n" | cryptsetup luksAddKey -q /dev/${iDEVICE}2
+					else
+							command printf "${sLUKS[1]}\n" | cryptsetup luksFormat -q -c aes-xts-plain64 -s 512 /dev/${iDEVICE}2
+						fi
+		          			command printf "${sLUKS[1]}\n" | cryptsetup luksOpen /dev/${iDEVICE}2 luksdev
+		    else
+		    		iLUKS="${iDEVICE}2"
+		    	fi
+		  	command mkfs.${iGENTOO[0]} -f -L gentoo /dev/${iLUKS}
+		    
+		  	if [ ${iGENTOO[0]} == "btrfs" ]
+		    	then
+		    		echo "Creating subvolumes..."
+						command mkdir /mnt/gentoo
+		        		command mount /dev/${iLUKS} /mnt/gentoo
+		        		command btrfs subvolume create /mnt/gentoo/@
+		        		command btrfs subvolume create /mnt/gentoo/@home
+		        		command umount -f /mnt/gentoo
+		        		command mount -t btrfs -o defaults,noatime,compress=zstd,discard=async,subvol=@ /dev/${iLUKS} /mnt/gentoo
+		        		stage3
+		        		command mount -t btrfs -o defaults,noatime,compress=zstd,discard=async,subvol=@home /dev/${iLUKS} /mnt/gentoo/home
+		  	else
+		    		command mount -t ${iGENTOO[0]} -o defaults,noatime /dev/${iLUKS} /mnt/gentoo
+		    		stage3
+		    	fi
+      		command mount /dev/${iDEVICE}1 /mnt/gentoo/boot
+      		if [ ${sLUKS[2]} == true ]
+		    	then
+		    		command cp crypt_key.luks.gpg /mnt/gentoo/boot
+		    	fi
+	}
+
+
+setupSystem ()
+	{
+    	command sed -i '/COMMON_FLAGS=/c\COMMON_FLAGS="-march=native -O2 -pipe"' /mnt/gentoo/etc/portage/make.conf
+	  	command echo "" >> /mnt/gentoo/etc/portage/make.conf
+	  	command echo "MAKEOPTS=\"${iMAKEOPTS}\"" >> /mnt/gentoo/etc/portage/make.conf
+    	command echo "USE=\"${iUSEFLAGS}\"" >> /mnt/gentoo/etc/portage/make.conf
+    	command echo 'ACCEPT_LICENSE="*"' >> /mnt/gentoo/etc/portage/make.conf
+    	command cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+		command cp ${sMOUNTPOINT}/gentoo_config-install_v${sSCRIPT_VERSION}.sh /mnt/gentoo
+		command cp ${sMOUNTPOINT}/gentoo_minimal-install_v${sSCRIPT_VERSION}.sh /mnt/gentoo
+		command cp ${sMOUNTPOINT}/gentoo_${iPROFILE[0]}-install_v${sSCRIPT_VERSION}.sh /mnt/gentoo
+		command sed -i "/gentoo_setup-install/c\iLUKS=${iLUKS}" /mnt/gentoo/gentoo_config-install_v${sSCRIPT_VERSION}.sh
+		command echo ". ./gentoo_minimal-install_v${sSCRIPT_VERSION}.sh" >> /mnt/gentoo/gentoo_config-install_v${sSCRIPT_VERSION}.sh
+    	command genfstab -U /mnt/gentoo >> /mnt/gentoo/etc/fstab
+  	}
+
+
+
+echo "Starting installation process..."
+formatDisk
+createFilesystem
+setupSystem
+
+
+arch-chroot /mnt/gentoo /bin/bash ./gentoo_config-install_v${sSCRIPT_VERSION}.sh
